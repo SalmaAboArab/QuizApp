36aedaa9047d19539461edfd1551b007930c2cf5
Lames-Atef <lameatef53@gmail.com>
2024-05-01 00:00:06 +0300

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ToastContainer theme='colored'/>
     <App />
   </React.StrictMode>,
 )
